31b0c02de133140abaf0ddc5ea5707d5a3340207
Sateesh Pinnamaneni <sqateez@gmail.com>
2014-11-14 22:59:02 +0000

    added mongo integration test with the help of embedded mongo db plug in and scala test maven plugin.

--- AuditObjects/audit-server/src/test/scala/com/thoughtstream/audit/service/MongoAuditMessageStoringServiceTest.scala
+++ AuditObjects/audit-server/src/test/scala/com/thoughtstream/audit/service/MongoAuditMessageStoringServiceTest.scala
@@ -1,20 +1,35 @@
 package com.thoughtstream.audit.service
 
+import com.github.simplyscala.{MongodProps, MongoEmbedDatabase}
+import com.mongodb.casbah.Imports._
+import com.mongodb.util.JSON
 import com.thoughtstream.audit.bean.MongoDB
 import com.thoughtstream.audit.process.JsonAuditMessageProcessor
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.json4s.native.JsonMethods._
 
 /**
  * Integration test
  * @author Sateesh
  * @since 13/11/2014
  */
-class MongoAuditMessageStoringServiceTest extends FunSuite {
+class MongoAuditMessageStoringServiceTest extends FunSuite with MongoEmbedDatabase with BeforeAndAfter{
+  var mongoProps: MongodProps = null
 
+  before {
+    mongoProps = mongoStart(27017)   // by default port = 12345 & version = Version.2.3.0
+  }                                  // add your own port & version parameters in mongoStart method if you need it
+
+  after { mongoStop(mongoProps) }
+
+  val serviceEndpoint = ("localhost",27017)
+  val databaseName = "AuditObjects"
 
   test("first record save to MongoDb") {
     val processor = JsonAuditMessageProcessor
-    val consumer = new MongoDB(("localhost",27017),"AuditObjects") with MongoAuditMessageStoringService
+    val consumer = new MongoDB(serviceEndpoint,databaseName) with MongoAuditMessageStoringService
+
+    val collection = MongoConnection(serviceEndpoint._1, serviceEndpoint._2)(databaseName)("defCollection")
 
     val oldObj = <entity name="user">
       <primitive name="eId" value="johnf"/>
@@ -30,5 +45,12 @@ class MongoAuditMessageStoringServiceTest extends FunSuite {
     </entity>
 
     consumer.save(processor.process(newObj, oldObj))
+    val userOptional = collection.findOne(JSON.parse("{'user.eId': 'johnf'}").asInstanceOf[DBObject])
+
+    val result = parse(userOptional.get.toString)
+    assert((result \ "user" \ "eId").values === "johnf")
+
+    //tear down
+    collection.remove(MongoDBObject())
   }
 }
